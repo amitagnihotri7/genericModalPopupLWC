e52f2c43f49926d87836848fcbe1c7647da3ec53
Amit Agnihotri <amit.agnihotri@salesforce.com>
2022-04-05 22:57:35 +0530

    newcommit

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -3,4 +3,4 @@ mkdir keys
 echo $CERT_KEY | base64 -di > server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid 3MVG9d8..z.hDcPJp1XxaHy.52_Bvi0AiHtFPZS5dNmy_SYOkAYujCXm_OUODo83VDDZVofR4LVtXXgqepfYb --jwtkeyfile server.key --username amitagnihotri7@gmail.com --instanceurl https://amitagnihotri-dev-ed.my.salesforce.com/
