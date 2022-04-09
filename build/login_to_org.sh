175c67c9e56c42fdc2c4b6e93ec78244688669a0
Amit Agnihotri <amit.agnihotri@salesforce.com>
2022-04-09 20:38:04 +0530

    longincommit

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -1,6 +1,6 @@
 echo "Logging into Salesforce Org"
 mkdir keys
-echo $CERT_KEY | base64 -di > server.key
+echo $CERT_KEY | base64 -di >  keys/server.key
 
 echo "Authenticating org"
-sfdx force:auth:jwt:grant --clientid 3MVG9d8..z.hDcPJp1XxaHy.52_Bvi0AiHtFPZS5dNmy_SYOkAYujCXm_OUODo83VDDZVofR4LVtXXgqepfYb --jwtkeyfile server.key --username amitagnihotri7@gmail.com --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
