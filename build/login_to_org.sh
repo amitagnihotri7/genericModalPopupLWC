385f88087ac70cc58ce7896352e2839c59d4cf08
Amit Agnihotri <amit.agnihotri@salesforce.com>
2022-04-09 22:24:19 +0530

    loginShellComit

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -1,6 +1,7 @@
 echo "Logging into Salesforce Org"
 mkdir keys
-echo $CERT_KEY | base64 -di >  keys/server.key
+echo $CERT_KEY | xxd -r -ps >> keys/server.key
+openssl rsa -in keys/server.key -check -noout
 
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
